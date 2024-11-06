d1361b3ed4f0dee6762400718807d077a05f5d3e
AWESOMEGUY5 <gusanal123@naver.com>
2024-11-06 22:41:29 +0900

    inital

---
+++ portone-jangsu/src/main/java/org/likelion/portoneskhu/payment/domain/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package org.likelion.portoneskhu.payment.domain.repository;
+
+
+import org.likelion.portoneskhu.payment.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+}
