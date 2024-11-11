12e2130fc945bd5132ec01fd699132ccc0fcc8ad
Lee, yeeun <ye7426@naver.com>
2024-11-11 16:49:22 +0900

    16주차 과제

---
+++ Leeyeeun/portone-skhu/src/main/java/org/likelion/portoneskhu/PortoneSkhuApplication.java
@@ -0,0 +1,13 @@
+package org.likelion.portoneskhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortoneSkhuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortoneSkhuApplication.class, args);
+    }
+
+}
